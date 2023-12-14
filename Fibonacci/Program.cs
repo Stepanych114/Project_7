0ebf3f9aea6ae3a520ca1579c977680bbc4e5f53
Stepanych114 <danikstepanyan.2015@mail.ru>
2023-12-14 15:05:42 +0300

    add Fib

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первые 6 чисел Фибоначчи:"+Fib(1) + " " + Fib(2) + " " + Fib(3) + " " + Fib(4) + " " + Fib(5) + " " + Fib(6)) ;
+        }
+        static int Fib(int n)
+        {
+            return (n < 2) ? n : Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
